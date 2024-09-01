00d99a854b2051a698e0a8585ac7b4fedae7c610
Dinges28 <39988228+Dinges28@users.noreply.github.com>
2024-09-01 21:43:37 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,18 +2,23 @@
 
 run()
 { 
+    # Set correct permissions for fetchmail configuration
     chmod 0600 /data/etc/fetchmailrc
     chown fetchmail:fetchmail /data/etc/
     chown fetchmail:fetchmail /data/etc/fetchmailrc
+    
+    # Prepare the log file for fetchmail
     touch /data/log/fetchmail.log
     chown fetchmail:fetchmail /data/log/fetchmail.log
-    # run cron daemon, which executes the logrotate job
+
+    # Run the cron daemon in the background (logrotate will be managed by cron)
     crond
-    # collect log informations for docker logs or docker-compose logs
+    
+    # Continuously output the last 50 lines of the log file to the console
     tail -n 50 -f /data/log/fetchmail.log &
-    # run fetchmail as endless loop with reduced permissions
+    
+    # Run fetchmail as an endless loop with reduced permissions
     su -s /bin/sh -c '/bin/sh /bin/fetchmail_daemon.sh' fetchmail
 }
 
 run
-
